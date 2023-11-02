d5e67acf509a7d5b84f9fc4c9a2f665d5b7f9686
edzordzinam44 <edzordzinam.alipui@gmail.com>
2023-11-01 19:18:55 -0700

    Task 3

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for j in range(ord('a'), ord('z') + 1):
+    # Check if the character is not 'q' and not 'e'
+    if chr(j) != 'q' and chr(j) != 'e':
+        print(chr(j), end='')
