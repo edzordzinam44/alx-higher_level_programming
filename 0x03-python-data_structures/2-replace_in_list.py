7c87e153f26cfd96837f3ac2cf2657cefea4d68f
edzordzinam44 <edzordzinam.alipui@gmail.com>
2023-11-06 16:31:18 -0800

    Replace an element

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def replace_in_listt(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    else:
+        my_list[idx] = element # Replace the element a he specified index
+        return my_list
