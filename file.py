e4638495cb011a791046c5ed938d33d95ce7507c
edzordzinam44 <edzordzinam.alipui@gmail.com>
2023-11-09 03:24:45 +0000

    My stuffs

--- file.py
+++ file.py
@@ -2,10 +2,17 @@ print("Print the mean word.\nI have hope.\n")
 print("I'm now learning python")
 my_list = [1, 2, 3, 4, 5]
 other_list = [7, 8, 9]
+result_list = [10, 11, 12, 13]
+class_list = [20, 21, 21, 21, 22, 23, 24, 25]
+
 my_list.remove(3)
-my_list.append(5)
-last_element = my_list.pop(1)
-print(last_element)
+append = my_list.append(5)
+pop = my_list.pop(2)
+count = class_list.count(21)
+clear = result_list.clear()
+
+#print(last_element)
+print(pop)
 
 #delete_at = __import__('file').delete_at
 
@@ -13,9 +20,14 @@ idx = 3
 new_list = FileExistsError(my_list, idx)
 print(new_list)
 print(my_list)
-my_list.extend(other_list)
+extend = my_list.extend(other_list)
 
-print(my_list)
+#print(my_list)
+print(result_list)
+print(count)
+print(append)
+print(pop)
+print(clear)
 
 a = 89
 b = 10
---
+++ file2.py
@@ -0,0 +1,3 @@
+my_list = [1, 2, 3, 2, 4, 5]
+insert = my_list.insert(3, 2)
+print(insert)
