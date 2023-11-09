0024a4d8c42d4861e70722fde3f196898d79af0d
edzordzinam44 <edzordzinam.alipui@gmail.com>
2023-11-09 04:19:33 +0000

    Print roman intergers

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,39 +1,12 @@
 #!/usr/bin/python3
+
+
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or not roman_string:
+    if not roman_string or type(roman_string) != str:
         return 0
-
-    valid_chars = set('IVXLCDM')
-
-    for char in roman_string:
-        if char not in valid_chars:
-            return 0
-
     total = 0
-
-    num_dict = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    }
-
-    i = 0
-    while i < len(roman_string):
-        a = num_dict[roman_string[i]]
-        if i + 1 < len(roman_string):
-            b = num_dict[roman_string[i + 1]]
-            if a < b:
-                total = total + (a - b)
-                i += 2
-            else:
-                total = total + a
-                i += 1
-        else:
-            total = total + a
-            i += 1
-
+    digits = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    for roman in reversed(roman_string):
+        arabic = digits[roman]
+        total += arabic if total < arabic * 5 else -arabic
     return total
