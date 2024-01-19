94a0a81cdcac60d7ff7098aefbb66afbb1c7c93b
edzordzinam44 <edzordzinam.alipui@gmail.com>
2024-01-19 08:55:47 +0000

    a script that creates the database hbtn_0c_0 in your MySQL server

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,5 +1,4 @@
--- deletes the database hbtn_0c_0 in your MySQL server.
--- If the database hbtn_0c_0 doesn't exist, your script should not fail
--- You are not allowed to use the SELECT or SHOW statements
+-- DELETES a database hbtn_0c_0 from the records
+-- If it doesn't exists, no error occurs
 
 DROP DATABASE IF EXISTS hbtn_0c_0;
