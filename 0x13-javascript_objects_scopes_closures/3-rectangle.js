eb538a47cceba19a7538e7a857872776a9c3cac5
edzordzinam44 <edzordzinam.alipui@gmail.com>
2024-02-19 15:59:34 -0800

    Update

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,19 +1,19 @@
 #!/usr/bin/node 
 
 const Rectangle = class {
-	constructor (w, h) {
-		if (w > 0 && h > 0) {
-			this.width = w;
-			this.height = h;
-		}
-	}
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 
-	print () {
-		while (this.height > 0) {
-			console.log('X'.repeat(this.width));
-			this.height--;
-		}
-	}
+  print () {
+    while (this.height > 0) {
+      console.log('X'.repeat(this.width));
+        this.height--;
+    }
+  }
 };
 
 module.exports = Rectangle;
