d767d81264dba814e474433a0934a507e03cc8fc
edzordzinam44 <edzordzinam.alipui@gmail.com>
2024-03-03 17:02:01 +0000

    Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """A Script takes in a letter and sends a POST request to:
-    "http://0.0.0.0:5000/search_user" with a letter as parameter
+"http://0.0.0.0:5000/search_user"
+with a letter as parameter
 """
 
 if __name__ == "__main__":
