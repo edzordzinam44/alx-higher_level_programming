fa0272f366c6d151323cfc0b12d1476d32dc7b46
edzordzinam44 <edzordzinam.alipui@gmail.com>
2024-03-03 16:38:19 +0000

    My Github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -3,11 +3,16 @@
 To display your "ID"
 """
 
-import sys
-import requests
-from requests.auth import HTTPBasicAuth
 
 if __name__ == "__main__":
-    auth = HTTPBasicAuth(sys.argv[1], sys.argv[1])
-    r = requests.get("https://api.github.com/user", auth=auth)
-    print(r.json().get("id"))
+    from requests import get
+    from sys import argv
+
+    username = argv[1]
+    password = argv[2]
+
+    URL = "https://api.github.com/user"
+    response = get(URL, auth=(username, password))
+    json = response.json()
+
+    print(json.get('id'))
