173649f837424b7d9c3a296a7c75244e76efc477
edzordzinam44 <edzordzinam.alipui@gmail.com>
2024-03-29 00:15:03 +0000

    Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,9 +4,9 @@ const request = require('request');
 const pathUrl = process.argv[2];
 
 request(pathUrl, (error, response, body) => {
-	if (error) {
-		console.error(error);
-	} else {
-		console.log(response.statusCode);
-	}
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(response.statusCode);
+  }
 });
